c81ca5753cb4dd73aebdc335997f322042c325a8
Inspiring-Games <inspiringgamestudio@gmail.com>
2025-10-22 11:31:30 +0200

    Added back obstacles

--- rainbowrace/RainbowRace.service.worker.js
+++ rainbowrace/RainbowRace.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761125357|480612405';
+const CACHE_VERSION = '1761125431|554810375';
 /** @type {string} */
 const CACHE_PREFIX = 'RainbowRace-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
