dda17dd0a3a82fd6479110b5f8da96270f51e22f
Inspiring-Games <inspiringgamestudio@gmail.com>
2025-10-24 18:48:17 +0200

    fix

--- rainbowrace/RainbowRace.service.worker.js
+++ rainbowrace/RainbowRace.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761301502|27651175';
+const CACHE_VERSION = '1761324488|44611600';
 /** @type {string} */
 const CACHE_PREFIX = 'RainbowRace-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
