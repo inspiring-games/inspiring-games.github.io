4b17ef919c58415424f74c143f959b75c416aaa8
Inspiring-Games <inspiringgamestudio@gmail.com>
2025-10-24 12:15:15 +0200

    Added shields, and explosions

--- rainbowrace/RainbowRace.service.worker.js
+++ rainbowrace/RainbowRace.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761125431|554810375';
+const CACHE_VERSION = '1761300903|323027443';
 /** @type {string} */
 const CACHE_PREFIX = 'RainbowRace-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
