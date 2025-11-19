742110a2f42bc4969310672475d8ac32739b6cfc
Inspiring-Games <inspiringgamestudio@gmail.com>
2025-11-19 09:49:45 +0100

    Fixed more memory leaks

--- rainbowrace/RainbowRace.service.worker.js
+++ rainbowrace/RainbowRace.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763495898|1168661769';
+const CACHE_VERSION = '1763542171|1367558965';
 /** @type {string} */
 const CACHE_PREFIX = 'RainbowRace-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
